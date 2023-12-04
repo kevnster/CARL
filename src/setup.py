47464a33e4707cc0654af600ac28aed986a2dda4
cosmochi <ababon590@gmail.com>
2023-12-03 19:58:18 -0500

    setup for scenes and navigation

--- src/setup.py
+++ src/setup.py
@@ -1,41 +1,78 @@
-from ai2thor.controller import Controller
 import matplotlib.pyplot as plt
+import random
+from ai2thor.controller import Controller
+from pynput import keyboard
+from pynput.keyboard import Key
 
-# Initialize the Controller
+available_scenes = [f"FloorPlan{i}" for i in range(210, 231)]
+
+# Randomly select a scene
+random_scene = random.choice(available_scenes)
+
 controller = Controller(
     agentMode="default",
-    scene="FloorPlan23",
-    gridSize=0.25,
-    snapToGrid=True,
-    rotateStepDegrees=90,
-    width=1280,
-    height=720,
+    visibilityDistance=1.5,
+    scene=random_scene,
+
+    # step sizes
+    gridSize=0.10,
+    snapToGrid=False,
+    rotateStepDegrees=10,
+
+    # image modalities
+    renderDepthImage=False,
+    renderInstanceSegmentation=False,
+
+    # camera properties
+    width=1600,
+    height=1200,
     fieldOfView=90
+
 )
+
 
 def show_image(event):
     plt.imshow(event.frame)
     plt.axis('off')
     plt.show()
 
-try:
-    while True:
-        # Display the current view
+
+def on_key_release(key):
+    try:
+        if key == Key.right:
+            print("Rotate Right")
+            event = controller.step("RotateRight")
+        elif key == Key.left:
+            print("Rotate Left")
+            event = controller.step("RotateLeft")
+        elif key == Key.up:
+            print("Move Ahead")
+            event = controller.step("MoveAhead")
+        elif key == Key.down:
+            print("Move Back")
+            event = controller.step("MoveBack")
+        elif key == Key.esc:
+            # Stop the listener
+            return False
+
+        # Update the display after an action
         show_image(controller.last_event)
 
-        # Get input from the user
-        action = input("Enter action (MoveAhead, RotateRight, RotateLeft, MoveBack, Exit): ")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-        # Perform the action
-        if action in ["MoveAhead", "RotateRight", "RotateLeft", "MoveBack"]:
-            event = controller.step(action)
-        elif action == "Exit":
-            break
-        else:
-            print("Invalid action.")
 
-except KeyboardInterrupt:
-    pass
+# Set up the listener
+listener = keyboard.Listener(on_release=on_key_release)
 
-# Stop the controller when done
-controller.stop()
+# Start the listener
+listener.start()
+
+# Keep the script running
+try:
+    listener.join()
+except Exception as e:
+    print(f"An error occurred: {e}")
+finally:
+    # Stop the controller when done
+    controller.stop()
